6a32aacb9f96b54460889cd152730d308ca22ee5
Jasmin Wilton <45803246+jwilton21@users.noreply.github.com>
2020-04-06 18:49:39 +0100

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,20 @@
+import java.sql.Driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+// Selenium test cases invokes browser testing.
+public class Demo {
+
+	public static void main(String[] args) 
+	{
+		// Implement methods of web driver. 
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\955089\\Documents\\chromedriver.exe");
+		// Create new driver object.
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://google.com");
+
+	}
+
+
+}
