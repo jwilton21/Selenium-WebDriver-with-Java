6c9595ffd2619d8a7ab78529e45e943b9c7c3449
Jasmin Wilton <45803246+jwilton21@users.noreply.github.com>
2020-05-11 17:30:13 +0100

    Updated web elements, name and css selector for booking.com. New updates navigate to booking.com, enter "Japan" and search all. Commented out some closing driver method until test cases are done.

--- Selenium/src/seleniumPackage/startBrowserTest.java
+++ Selenium/src/seleniumPackage/startBrowserTest.java
@@ -2,9 +2,12 @@
 package seleniumPackage;
 import org.junit.BeforeClass;
 import org.junit.AfterClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.Test;
+import java.util.concurrent.TimeUnit;
 
 public class startBrowserTest {
 	// Declare webDriver object. Variable access within class only and static
@@ -22,12 +25,23 @@ public class startBrowserTest {
 	@Test
 	public void  testChromeSelenium() 
 	{	// Try catch block to catch potential exception thrown 
-		// and print stackTrace errors to console if occured.
+		// and print stackTrace errors to console if occurred.
 		try 
 		{	// Go to url.
-			driver.get("https://google.com");   
+			driver.get("https://www.booking.com/");
+			// Return to console webpage title.
+			System.out.println("Web page title is:" +driver.getTitle());
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			// Maximise window.
+			driver.manage().window().maximize();
+			// Search button has web element name 'ss'.Send 'Japan to element.
+			driver.findElement(By.name("ss")).sendKeys("Japan");
+			// Click on search button element.
+			driver.findElement(By.cssSelector("#frm > div.xp__fieldset.accommodation > div.xp__button > div.sb-searchbox-submit-col.-submit-button > button > span:nth-child(1)")).click();
+			
+			// If exception thrown, catch here.
 		}catch (Exception e) 
-		{
+		{	//Call to print stack trace method so we can get the line that caused exception.
 			e.printStackTrace();
 		}
 
@@ -35,12 +49,13 @@ public class startBrowserTest {
 	// AfterClass method executed after all class Tests.
 	@AfterClass
 	public static void cleanUp() 
-	// If driver object != null then close driver (webDriver object)
-	{	if(driver != null) {
-		driver.close();
-		driver.quit();		
-	}
-
+	{	/* Commented out this stuff until test cases are complete. Then we can close driver.
+		If driver object != null then close driver (webDriver object)
+		if(driver != null) 
+			Close Chrome.
+			driver.close();
+		//driver.quit();	
+	 */
 	}
 }
 
